3d8b7b5c4700c2f477de0f57e30ef6485dafa9c4
Daun Jung <12junghoon@gmail.com>
2020-08-24 10:56:31 +0900

    develop ver2

--- src/main/java/edu/bit/ex/HomeController2.java
+++ src/main/java/edu/bit/ex/HomeController2.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 //ver1
+//ver2
 @Controller
 public class HomeController2 {
 	
